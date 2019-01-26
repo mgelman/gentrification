da35554e8ce5484be97317a140c88b8d57825d7b
mgelman <mgelman@cmc.edu>
2019-01-26 14:23:58 -0800

    Update review_classification.py

--- review_classification.py
+++ review_classification.py
@@ -8,6 +8,8 @@ Created on Fri Jan 25 17:11:08 2019
 
 
 import pandas as pd
+import itertools
+import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
@@ -32,18 +34,14 @@ sys.path.insert(0, "C:\Users\mgelman\AppData\Local\Continuum\anaconda2\Library\b
 
 #STEP 1: LOADING IN DATA - I ALREADY HAVE THE DATA READY TO GO FROM STATA
 #inputfile=os.path.join(path,"review.json")
-inputfile=os.path.join(path,"review_100k.json")
+inputfile=os.path.join(path,"review_50k.json")
 outputfile=os.path.join(path,"review.pkl")
 
 # load in file
-data = pd.read_json(inputfile,lines=True) #python engine is slower than c but i'm having some trouble with some of the chars
-
+data = pd.read_json(inputfile,lines=True) 
 
 #only keep stars and text
 data=data[['stars','text']]
-
-
-#maybe think about taking any #s out?
 
 
 #THIS SECTION WHEN LOADING DATA TO PREDICT
@@ -53,6 +51,9 @@ data=data[['stars','text']]
 y_data=data['stars']
 X_data=data['text']
 print y_data.value_counts(normalize=True, sort=False)
+
+#create label
+star_label = [1,2,3,4,5]
 
 #split into training and test
 X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.25, random_state=1234)
@@ -71,7 +72,50 @@ X_te = vectorizer.transform(X_test)
 vocab = vectorizer.get_feature_names()
 vocab_str = [str(x.encode('utf-8')) for x in vocab]
 
+
+
+
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+
+
+
 #%%
+
 
 
 #Use the tree clasifier
@@ -99,9 +143,17 @@ y_pred = clf.predict(X_te)
 
 #confusion matrix
 cm=confusion_matrix(y_test, y_pred)
+np.set_printoptions(precision=2)
 
-print cm
-print cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+plt.figure()
+
+plot_confusion_matrix(cm, classes=star_label,normalize=True,title='Normalized confusion matrix')
+
+plt.show()
+
+
+#print cm
+#print cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
 #overall score
 training_score = clf.score(X_tr, y_train, sample_weight = None)
@@ -112,15 +164,46 @@ print()
 print("the testing_score is " + str(testing_score))
 
 
-
 #best features
 dictionary = dict(zip(vocab_str,clf.feature_importances_))
 sorted_x = sorted(dictionary.items(), key=operator.itemgetter(1),reverse=True)
 for x in range(10):
     print sorted_x[x]
 
+#%%
+from sklearn.naive_bayes import MultinomialNB    
+    
+clf = MultinomialNB().fit(X_tr, y_train) #classifying transformed text data to target value 
+
+#confusion matrix
+y_pred = clf.predict(X_te)
+cm=confusion_matrix(y_test, y_pred
+                    )
+print cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
 
+#calculating the mean accuracy on the given test data and labels 
+training_score = clf.score(X_tr, y_train, sample_weight = None)
+testing_score = clf.score(X_te, y_test, sample_weight = None)
+print()
+print("the training_score is " + str(training_score))
+print()
+print("the testing_score is " + str(testing_score))
+
+#%%
+from sklearn.neural_network import MLPClassifier
+    
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
+
+clf = clf.fit(X_tr, y_train)
+
+#calculating the mean accuracy on the given test data and labels 
+training_score = clf.score(X_tr, y_train, sample_weight = None)
+testing_score = clf.score(X_te, y_test, sample_weight = None)
+print()
+print("the training_score is " + str(training_score))
+print()
+print("the testing_score is " + str(testing_score))
 
 #%% Compare all the different types 
     
@@ -136,7 +219,7 @@ from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 classifiers = [
     KNeighborsClassifier(3),
     LinearSVC(),
-    NuSVC(probability=True),
+    #NuSVC(probability=True),
     GaussianNB(),
     MultinomialNB(),
     LinearDiscriminantAnalysis(),
@@ -182,34 +265,9 @@ print("="*30)
 print log.sort_values(by='Accuracy',ascending=False)
 
 
-#%%
-from sklearn.naive_bayes import MultinomialNB    
-    
-clf = MultinomialNB().fit(X_tr, y_train) #classifying transformed text data to target value 
-
-#calculating the mean accuracy on the given test data and labels 
-training_score = clf.score(X_tr, y_train, sample_weight = None)
-testing_score = clf.score(X_te, y_test, sample_weight = None)
-print()
-print("the training_score is " + str(training_score))
-print()
-print("the testing_score is " + str(testing_score))
 
 
-#%%
-from sklearn.neural_network import MLPClassifier
-    
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
 
-clf = clf.fit(X_tr, y_train)
-
-#calculating the mean accuracy on the given test data and labels 
-training_score = clf.score(X_tr, y_train, sample_weight = None)
-testing_score = clf.score(X_te, y_test, sample_weight = None)
-print()
-print("the training_score is " + str(training_score))
-print()
-print("the testing_score is " + str(testing_score))
 
 #%%
 #Graph the tree
